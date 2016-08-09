360e4fa66a21ca0cfd2001bbdd988742e601d8f3
Dong Zou <jiwenzou@gmail.com>
2016-08-09 12:14:14 -0700

    123

---
+++ src/day1/robot/intro.java
@@ -0,0 +1,20 @@
+package day1.robot;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class intro {
+
+	public static void main(String[] args) {
+		Robot Bobby = new Robot();
+		Bobby.turn(90);
+		Bobby.move(700);
+		Bobby.turn(270);
+		Bobby.move(400);
+		Bobby.turn(270);
+		Bobby.move(1400);
+		Bobby.turn(208);
+		Bobby.move(1550);
+
+	}
+
+}
